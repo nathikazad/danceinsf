5ccd299e696e26758526d48f5ff17b8318edfdeb
Nathik Azad <nathikazad@gmail.com>
2025-05-19 18:57:49 -0700

    app drawer

--- flutter_application/lib/widgets/list_event_widgets/app_drawer.dart
+++ flutter_application/lib/widgets/list_event_widgets/app_drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AppDrawer extends ConsumerWidget {
   const AppDrawer({super.key});
@@ -44,7 +45,12 @@ class AppDrawer extends ConsumerWidget {
                   style: Theme.of(context).textTheme.labelLarge,
                 ),
                 onTap: () {
-                  // TODO: Implement Contact navigation
+                  // send email to nathikazad@gmail.com
+                  final email = 'nathikazad@gmail.com';
+                  final subject = 'Contact';
+                  final body = 'Hello, I have a question about the event.';
+                  final url = 'mailto:$email?subject=$subject&body=$body';
+                  launchUrl(Uri.parse(url));
                   Navigator.of(context).pop();
                 },
               ),
@@ -52,60 +58,60 @@ class AppDrawer extends ConsumerWidget {
                 height: 25,
                 child: Divider(),
               ),
-              ListTile(
-                leading: Container(
-                    padding: const EdgeInsets.all(8),
-                    decoration: BoxDecoration(
-                        color: Theme.of(context)
-                            .colorScheme
-                            .primary
-                            .withOpacity(0.1),
-                        borderRadius: BorderRadius.circular(100)),
-                    child: Icon(
-                      Icons.chat,
-                      size: 25,
-                      color: Theme.of(context).colorScheme.primary,
-                    )),
-                title: Text(
-                  'Groupchat',
-                  style: Theme.of(context).textTheme.labelLarge,
-                ),
-                onTap: () {
-                  // TODO: Implement Groupchat navigation
-                  Navigator.of(context).pop();
-                },
-              ),
-              SizedBox(
-                height: 25,
-                child: Divider(),
-              ),
-              ListTile(
-                leading: Container(
-                    padding: const EdgeInsets.all(8),
-                    decoration: BoxDecoration(
-                        color: Theme.of(context)
-                            .colorScheme
-                            .primary
-                            .withOpacity(0.1),
-                        borderRadius: BorderRadius.circular(100)),
-                    child: Icon(
-                      Icons.edit,
-                      size: 25,
-                      color: Theme.of(context).colorScheme.primary,
-                    )),
-                title: Text(
-                  'Edits',
-                  style: Theme.of(context).textTheme.labelLarge,
-                ),
-                onTap: () {
-                  // TODO: Implement Edits navigation
-                  Navigator.of(context).pop();
-                },
-              ),
-              SizedBox(
-                height: 25,
-                child: Divider(),
-              ),
+              // ListTile(
+              //   leading: Container(
+              //       padding: const EdgeInsets.all(8),
+              //       decoration: BoxDecoration(
+              //           color: Theme.of(context)
+              //               .colorScheme
+              //               .primary
+              //               .withOpacity(0.1),
+              //           borderRadius: BorderRadius.circular(100)),
+              //       child: Icon(
+              //         Icons.chat,
+              //         size: 25,
+              //         color: Theme.of(context).colorScheme.primary,
+              //       )),
+              //   title: Text(
+              //     'Groupchat',
+              //     style: Theme.of(context).textTheme.labelLarge,
+              //   ),
+              //   onTap: () {
+              //     // TODO: Implement Groupchat navigation
+              //     Navigator.of(context).pop();
+              //   },
+              // ),
+              // SizedBox(
+              //   height: 25,
+              //   child: Divider(),
+              // ),
+              // ListTile(
+              //   leading: Container(
+              //       padding: const EdgeInsets.all(8),
+              //       decoration: BoxDecoration(
+              //           color: Theme.of(context)
+              //               .colorScheme
+              //               .primary
+              //               .withOpacity(0.1),
+              //           borderRadius: BorderRadius.circular(100)),
+              //       child: Icon(
+              //         Icons.edit,
+              //         size: 25,
+              //         color: Theme.of(context).colorScheme.primary,
+              //       )),
+              //   title: Text(
+              //     'Edits',
+              //     style: Theme.of(context).textTheme.labelLarge,
+              //   ),
+              //   onTap: () {
+              //     // TODO: Implement Edits navigation
+              //     Navigator.of(context).pop();
+              //   },
+              // ),
+              // SizedBox(
+              //   height: 25,
+              //   child: Divider(),
+              // ),
               if (ref.watch(authProvider).state.user != null)
                 ListTile(
                   leading: Container(
