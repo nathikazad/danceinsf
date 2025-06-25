55f19096f8d1208d6895189703b9d5d711b8830f
Nathik Azad <nathikazad@gmail.com>
2025-06-24 19:44:39 -0600

    sidebar collapse button wokring

--- learning_app/lib/widgets/sidebar_navigation.dart
+++ learning_app/lib/widgets/sidebar_navigation.dart
@@ -13,19 +13,21 @@ class SidebarToggleButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: 40,
-      height: 40,
+      width: 20,
+      height: 20,
       child: IconButton(
         icon: Icon(
           isSidebarVisible ? Icons.chevron_left : Icons.chevron_right,
           color: Colors.black87,
+          size: 16,
         ),
         onPressed: onToggle,
         style: IconButton.styleFrom(
           backgroundColor: Colors.white,
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8),
+            borderRadius: BorderRadius.circular(4),
           ),
+          padding: EdgeInsets.zero,
         ),
       ),
     );
@@ -49,44 +51,64 @@ class SidebarSection extends StatefulWidget {
 }
 
 class _SidebarSectionState extends State<SidebarSection> {
-  bool isSidebarVisible = true;
+  bool _isSidebarVisible = true;
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        AnimatedContainer(
-          duration: const Duration(milliseconds: 300),
-          width: isSidebarVisible ? 200 : 0,
-          child: isSidebarVisible
-              ? Container(
-                  width: 200,
-                  color: Colors.grey[300],
-                  child: ListView.builder(
-                    padding: const EdgeInsets.all(8),
-                    itemCount: widget.sections.length,
-                    itemBuilder: (context, index) {
-                      final isSelected = widget.selectedIndex == index;
-                      return Card(
-                        margin: const EdgeInsets.symmetric(vertical: 4),
-                        child: ListTile(
-                          title: Text(
-                            widget.sections[index],
-                            style: TextStyle(
-                              fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
-                            ),
+    return Container(
+      width: _isSidebarVisible ? 200 : 30,
+      child: Stack(
+        children: [
+          // Sidebar content
+          AnimatedContainer(
+            duration: const Duration(milliseconds: 300),
+            width: 200,
+            child: Transform.translate(
+              offset: Offset(_isSidebarVisible ? 0 : -170, 0),
+              child: Container(
+                width: 200,
+                color: Colors.grey[300],
+                child: ListView.builder(
+                  padding: const EdgeInsets.all(8),
+                  itemCount: widget.sections.length,
+                  itemBuilder: (context, index) {
+                    final bool isSelected = widget.selectedIndex == index;
+                    return Card(
+                      margin: const EdgeInsets.symmetric(vertical: 4),
+                      child: ListTile(
+                        title: Text(
+                          widget.sections[index],
+                          style: TextStyle(
+                            fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
                           ),
-                          onTap: () {
-                            widget.onSectionSelected(index);
-                          },
                         ),
-                      );
-                    },
-                  ),
-                )
-              : null,
-        ),
-      ],
+                        onTap: () {
+                          widget.onSectionSelected(index);
+                        },
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ),
+          // Toggle button
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 300),
+            right: _isSidebarVisible ? 0 : null,
+            left: _isSidebarVisible ? null : 0,
+            top: 0,
+            child: SidebarToggleButton(
+              isSidebarVisible: _isSidebarVisible,
+              onToggle: () {
+                setState(() {
+                  _isSidebarVisible = !_isSidebarVisible;
+                });
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
-} 
+}
