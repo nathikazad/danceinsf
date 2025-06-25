539d7be5da8cbeb709cab72291c81f38531d5a24
Nathik Azad <nathikazad@gmail.com>
2025-06-24 23:01:56 -0600

    accordion working

--- learning_app/lib/main.dart
+++ learning_app/lib/main.dart
@@ -23,14 +23,84 @@ final List<String> videoUrls = [
   movieFive,
 ];
 
-class VideoApp extends StatefulWidget {
-  const VideoApp({super.key});
+class AccordionWidget extends StatefulWidget {
+  final String title;
+  final Widget child;
+  final bool initiallyExpanded;
+
+  const AccordionWidget({
+    super.key,
+    required this.title,
+    required this.child,
+    this.initiallyExpanded = false,
+  });
 
   @override
-  _VideoAppState createState() => _VideoAppState();
+  State<AccordionWidget> createState() => _AccordionWidgetState();
 }
 
-class _VideoAppState extends State<VideoApp> {
+class _AccordionWidgetState extends State<AccordionWidget> {
+  bool _isExpanded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _isExpanded = widget.initiallyExpanded;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text(
+              widget.title,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+            trailing: Icon(
+              _isExpanded ? Icons.expand_less : Icons.expand_more,
+            ),
+            onTap: () {
+              setState(() {
+                _isExpanded = !_isExpanded;
+              });
+            },
+          ),
+          AnimatedCrossFade(
+            firstChild: const SizedBox.shrink(),
+            secondChild: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: widget.child,
+            ),
+            crossFadeState: _isExpanded
+                ? CrossFadeState.showSecond
+                : CrossFadeState.showFirst,
+            duration: const Duration(milliseconds: 300),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class VideoPlayerWidget extends StatefulWidget {
+  final List<String> videoUrls;
+
+  const VideoPlayerWidget({
+    super.key,
+    required this.videoUrls,
+  });
+
+  @override
+  State<VideoPlayerWidget> createState() => _VideoPlayerWidgetState();
+}
+
+class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
   VideoPlayerController? _controller;
   ChewieController? _chewieController;
   int _currentIndex = 0;
@@ -62,7 +132,7 @@ class _VideoAppState extends State<VideoApp> {
     _chewieController?.dispose();
     _controller?.dispose();
 
-    _controller = VideoPlayerController.networkUrl(Uri.parse(videoUrls[index]));
+    _controller = VideoPlayerController.networkUrl(Uri.parse(widget.videoUrls[index]));
     await _controller!.initialize();
 
     _chewieController = ChewieController(
@@ -87,6 +157,70 @@ class _VideoAppState extends State<VideoApp> {
 
   @override
   Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 120,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: widget.videoUrls.length,
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () => _changeVideo(index),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: AspectRatio(
+                    aspectRatio: 16 / 9,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: _currentIndex == index
+                              ? Colors.blue
+                              : Colors.transparent,
+                          width: 3.0,
+                        ),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(5),
+                        child: Image.network(
+                          _getThumbnailUrl(widget.videoUrls[index]),
+                          fit: BoxFit.cover,
+                          errorBuilder: (context, error, stackTrace) =>
+                              const Icon(Icons.error),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+        Expanded(
+          child: Center(
+            child: _isLoading || _chewieController == null
+                ? const CircularProgressIndicator()
+                : Chewie(
+                    controller: _chewieController!,
+                  ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class VideoApp extends StatefulWidget {
+  const VideoApp({super.key});
+
+  @override
+  _VideoAppState createState() => _VideoAppState();
+}
+
+class _VideoAppState extends State<VideoApp> {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Video Player Demo',
       debugShowCheckedModeBanner: false,
@@ -94,56 +228,33 @@ class _VideoAppState extends State<VideoApp> {
         appBar: AppBar(
           title: const Text('Video Player Demo'),
         ),
-        body: Column(
-          children: [
-            SizedBox(
-              height: 120,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: videoUrls.length,
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: () => _changeVideo(index),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: AspectRatio(
-                        aspectRatio: 16 / 9,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: _currentIndex == index
-                                  ? Colors.blue
-                                  : Colors.transparent,
-                              width: 3.0,
-                            ),
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(5),
-                            child: Image.network(
-                              _getThumbnailUrl(videoUrls[index]),
-                              fit: BoxFit.cover,
-                              errorBuilder: (context, error, stackTrace) =>
-                                  const Icon(Icons.error),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  );
-                },
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              AccordionWidget(
+                title: 'Video Player 1',
+                initiallyExpanded: true,
+                child: SizedBox(
+                  height: 400,
+                  child: VideoPlayerWidget(videoUrls: videoUrls),
+                ),
               ),
-            ),
-            Expanded(
-              child: Center(
-                child: _isLoading || _chewieController == null
-                    ? const CircularProgressIndicator()
-                    : Chewie(
-                        controller: _chewieController!,
-                      ),
+              AccordionWidget(
+                title: 'Video Player 2',
+                child: SizedBox(
+                  height: 400,
+                  child: VideoPlayerWidget(videoUrls: videoUrls.reversed.toList()),
+                ),
               ),
-            ),
-          ],
+              AccordionWidget(
+                title: 'Video Player 3',
+                child: SizedBox(
+                  height: 400,
+                  child: VideoPlayerWidget(videoUrls: [movieOne, movieThree, movieFive]),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
