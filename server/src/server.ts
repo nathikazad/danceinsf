2dd7d7ff08fc01601e1917985a4de854230757a7
Nathik Azad <nathikazad@gmail.com>
2025-06-25 19:43:28 -0600

    added cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -15,6 +15,19 @@ const wss = new WebSocketServer({ server });
 
 app.use(express.json());
 
+// Add CORS middleware
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
 // List of reserved paths (add any other endpoints you want to exclude from redirect)
 const reserved = [
     'parseUserRequestFromAudio',
